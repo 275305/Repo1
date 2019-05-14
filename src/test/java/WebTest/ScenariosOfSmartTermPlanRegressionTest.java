02f498ac90908ba8e592956b4deedee6987ae2a4
AzureAD\NavneetKumar <navneet@monnocept.com>
2019-05-14 16:20:41 +0530

    functional test

--- src/test/java/WebTest/ScenariosOfSmartTermPlanRegressionTest.java
+++ src/test/java/WebTest/ScenariosOfSmartTermPlanRegressionTest.java
@@ -8,9 +8,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 
 import uielements.HomePage;
 import uielements.JourneyScreenTwo;
@@ -19,46 +21,9 @@ import uielements.ReusableActions;
 
 public class ScenariosOfSmartTermPlanRegressionTest extends ReusableActions {
 
-	@BeforeTest
-	@Parameters(value = { "browser", "version", "platform", "testrun" })
-	public void setUp(String browser, String version, String platform, String testrun) throws Exception {
-		String testRun = testrun;
-		/* String testRun1=testrun1; */
-		if (testRun.equals("run")) {
-			// if (testRun.equals("runbsw")) {
-
-			try {
-				DesiredCapabilities capability = new DesiredCapabilities();
-				capability.setCapability("platform", platform);
-				capability.setCapability("browserName", browser);
-				capability.setCapability("browserVersion", version);
-				// capability.setCapability("browserstack.local", "true");
-				capability.setCapability("browserstack.debug", "true");
-				capability.setCapability("project", "MaxlifeInsurance");
-				capability.setCapability("build", "BrowserStack_WebSeries1");
-				driver = new RemoteWebDriver(
-						new URL("https://kanchangupta2:xWbVkws9u8oEYR1Nsqx6@hub-cloud.browserstack.com/wd/hub"),
-						capability);
-				driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-				ReusableActions.logStep("=========Execution Started on Browsers=========");
-				System.out.println("Execution Started on : " + browser + " " + version + " " + platform);
-				ReusableActions.attachScreen(driver);
-			} catch (Exception e) {
-				// System.out.println("Test case failed: " + e.getMessage());
-				throw e;
-			}
-			// break;
-		}
-
-		else
-		if (testRun.equals("runfa")) {
-			// if (testRun.equals("run")) {
-			// @BeforeClass
-			// public void launchBrowser() throws Exception{
-
-			try {
-				// System.setProperty("webdriver.chrome.driver",
-				// "D:\\chromedriver_win32\\chromedriver.exe");
+	//@BeforeTest
+	public void abc() throws Exception{
+	try{
 				System.setProperty("webdriver.chrome.driver",
 						"C:\\Matrix\\AutomationQA\\automationqa\\src\\test\\resources\\driver\\chromedriver.exe");
 				driver = new ChromeDriver();
@@ -68,7 +33,37 @@ public class ScenariosOfSmartTermPlanRegressionTest extends ReusableActions {
 			}
 
 		}
-	}
+	
+	
+	
+	@BeforeClass	
+    public void launchBrowser() throws Exception{
+	 try {
+    System.setProperty("webdriver.chrome.driver", ".\\lib\\Drivers\\chromedriver.exe");
+    driver = new ChromeDriver();
+    
+    logger.info("Test Started");
+	prop = ReusableActions.readProperties();
+
+ 	driver.get(prop.getProperty("SampleURL"));
+ 	LoginPage.verifyloginpage();
+ 			
+ 	// Maximizing the browser window
+ 	driver.manage().window().maximize();
+ 	driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+ 	logger.info("Fulfilment URL opened successfully");
+ 	
+ 	LoginPage.logintest(prop.getProperty("username"), prop.getProperty("password"));
+ 	logger.info("User logged in successfully");
+	 }
+	 catch (Exception e) {
+ 			logger.error("Test case failed: " + e.getMessage());
+ 			throw e;
+ 		} 
+
+    }
+	
+	
 
 	@Test(priority = 0, enabled = true)
 	public void India() throws Exception {
