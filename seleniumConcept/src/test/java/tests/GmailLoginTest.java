1359d7bf6db05c10587674487ce3e5f16c24ee02
275305 <pradeephbtu@gmail.com>
2019-10-11 19:49:42 +0530

    Reading the Data from Apache POI

--- seleniumConcept/src/test/java/tests/GmailLoginTest.java
+++ seleniumConcept/src/test/java/tests/GmailLoginTest.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
@@ -9,20 +12,31 @@ import pages.GmailLoginPage;
 import pages.PropertyReader;
 
 public class GmailLoginTest extends LaunchBrowser {
-	
+
 	PropertyReader reader = new PropertyReader();
 
 	@Test(priority = 0)
-	public void gmailLoginTest()  {
+	public void gmailLoginTest() {
 		GmailLoginPage gmailPage = PageFactory.initElements(driver, GmailLoginPage.class);
 		try {
-			gmailPage.emailText(reader.getEmailText());
-			gmailPage.emailPassword(reader.getEmailPassword());
+			int sheet = 0, row = 0, column = 0;
+			List<Integer> list = reader.getEmailTextValue();
+			Iterator<Integer> itr = list.iterator();
+
+			while (itr.hasNext()) {
+				sheet = itr.next();
+				row = itr.next();
+				column = itr.next();
+			}
+
+			gmailPage.emailIdTextGmailLoginPage(sheet, row, column);
+			// gmailPage.emailText(reader.getEmailText());
+			// gmailPage.emailPassword(reader.getEmailPassword());
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-              
+
 	}
 
 }
