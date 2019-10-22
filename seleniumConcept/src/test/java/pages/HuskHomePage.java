ccff3c6f8e4fdea6457d3b76c7be209153459302
275305 <pradeephbtu@gmail.com>
2019-10-22 19:07:46 +0530

    Added log4j.properties file

---
+++ seleniumConcept/src/test/java/pages/HuskHomePage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HuskHomePage {
+	
+	WebDriver driver;
+
+	public HuskHomePage(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
+	
+	
+	
+
+}
--- seleniumConcept/src/test/java/util/ReusableActions.java
+++ seleniumConcept/src/test/java/util/ReusableActions.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -20,48 +22,45 @@ import org.testng.annotations.BeforeTest;
 import pages.PropertyReader;
 
 public class ReusableActions {
-	
-protected static WebDriver driver;
 
-Properties properties;
-String PATH = System.getProperty("user.dir");
-private final String propertyFilePath = PATH + "\\config\\configuration.properties";;
+	protected static WebDriver driver;
+	private static final Logger lOGGER = Logger.getLogger(ReusableActions.class.getName());
+	Properties properties;
+	String PATH = System.getProperty("user.dir");
+	private final String propertyFilePath = PATH + "\\config\\configuration.properties";;
+	PropertyReader reader = new PropertyReader();
 
-// PATH + "\\config\\configration.properties"
-PropertyReader reader=new PropertyReader();
-	
- public ReusableActions() {
-	// TODO Auto-generated constructor stub
-
-	BufferedReader reader;
-
-	try {
-
-		reader = new BufferedReader(new FileReader(propertyFilePath));
-		properties = new Properties();
+	public ReusableActions() {
+		BufferedReader reader;
 		try {
-			properties.load(reader);
-			reader.close();
-		} catch (IOException e) {
+			reader = new BufferedReader(new FileReader(propertyFilePath));
+			properties = new Properties();
+			try {
+				properties.load(reader);
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
+			throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
 		}
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-		throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
 	}
-}
 
 	@BeforeTest
 	public void initBrowser() {
-		//String PATH = System.getProperty("user.dir");
-		//PATH + "\\lib\\chromedriver.exe"
-		System.setProperty("webdriver.chrome.driver",reader.getChromeDriver());
+		/* String PATH=System.getProperty("user.dir");
+		PropertyConfigurator.configure(PATH+ "\\tests\\log4j.properties");*/
+		System.setProperty("webdriver.chrome.driver", reader.getChromeDriver());
 		driver = new ChromeDriver();
 		String url = reader.getApplicationUrl();
 		driver.manage().window().maximize();
+		lOGGER.info("window is maximized");
 		driver.manage().timeouts().implicitlyWait(reader.getImplicitWait(), TimeUnit.SECONDS);
+		lOGGER.info("used the implecit wait");
 		driver.get(url);
-			}
+		lOGGER.info("url passed sucessfully");
+	}
 
 	public static String readingdata(int sheetno, int rownum, int colnum) throws Exception {
 		File file = new File(System.getProperty(AppConstant.USER_DIR) + AppConstant.MASTER_DATA_EXCELL);
